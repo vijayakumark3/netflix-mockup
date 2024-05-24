5bc12ae812c1548ca73aee9c1d9c381002af3842
Vijayakumar K <101494237+vijayakumark3@users.noreply.github.com>
2024-05-24 19:49:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='../logomain.png' className="App-logo" alt="logo" />
+
+        <ul>
+          <li>
+            <select>
+              <option>English</option>
+              <option>Hindi</option>
+            </select>
+          </li>
+          <li><a href='#'>Sign In</a></li>
+        </ul>
+
+        <div className='clrdiv'></div>
       </header>
+
+      <div className='banner'>
+        <div className='content1'>
+          <h1>Unlimited movies, TV shows and more</h1>
+          <p>Watch anywhere. Cancel anytime.</p>
+          <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
